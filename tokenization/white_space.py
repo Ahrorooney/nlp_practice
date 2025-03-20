9087001942c967f87957c4d95a3babedacb3fd79
a_gaibnazarov <a.gaibnazarov@uzinfocom.uz>
2025-03-20 09:10:15 +0300

    - white_space tokenization

---
+++ tokenization/white_space.py
@@ -0,0 +1,8 @@
+#sample text
+text = 'Tokenization splits text into words, phrases, symbols or other meaningful elements, which are called tokens.'
+
+# White Space Tokenization using split()
+tokens = text.split()
+
+# Print tokens
+print(tokens)
